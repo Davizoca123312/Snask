f678a194de1ecd59031ba5638429cc58e1837375
Davizoca123312 <davivbrr@gmail.com>
2025-04-26 15:17:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
         with app.app_context():
-            init_db()  # <- sempre tenta garantir a tabela
+            
     return db
 
 def init_db():
