3674cc06873078f4997bc87778f50b652f0c93e8
Davizoca123312 <davivbrr@gmail.com>
2025-04-26 15:18:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-        with app.app_context():
+        
             
     return db
 
